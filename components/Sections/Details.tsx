a9cf19b00050a9cd36c0baa33d1c629a85b797e9
Tomáš Magnes <tomas.magnes@gmail.com>
2024-01-08 16:22:07 +0100

    &apos; fix for compilation

--- components/Sections/Details.tsx
+++ components/Sections/Details.tsx
@@ -14,7 +14,7 @@ const Details = () => {
             <div className="flex-1 flex flex-col gap-4 justify-center items-start">
                 <p className="text-blue-500 text-sm font-bold uppercase">About</p>
                 <h2 className="text-4xl font-bold">PC Control with ESP32 & Port Forwarding</h2>
-                <p className="text-gray-500">Our service is dedicated to providing seamless and efficient control of your computer systems from anywhere in the world. Harnessing the power of the ESP32 microcontroller alongside the Wake-on-LAN (WOL) protocol, and integrating advanced port forwarding capabilities, we've crafted a comprehensive solution that allows you to awaken, access, and manage your PCs remotely.</p>
+                <p className="text-gray-500">Our service is dedicated to providing seamless and efficient control of your computer systems from anywhere in the world. Harnessing the power of the ESP32 microcontroller alongside the Wake-on-LAN (WOL) protocol, and integrating advanced port forwarding capabilities, we&apos;ve crafted a comprehensive solution that allows you to awaken, access, and manage your PCs remotely.</p>
                 <p className="text-gray-500"> Port forwarding feature is pivotal for users operating within complex network setups or behind multiple layers of network security. Our approach ensures that you can remotely power up and access your computer with ease and confidence, making remote management more accessible and reliable than ever.</p>
             </div>
             <div className="flex-1 flex justify-end items-center pt-8 md:pt-0">
