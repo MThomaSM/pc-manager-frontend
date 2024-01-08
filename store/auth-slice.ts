943bef7d1a0db188bd7364feef46633ea0f81ce6
Tomáš Magnes <tomas.magnes@gmail.com>
2024-01-08 16:26:36 +0100

    Revert "Updated auth slice for new next version."
    This reverts commit b65d6152c282b7b7f3f8592b3f557b29180bace0.

--- store/auth-slice.ts
+++ store/auth-slice.ts
@@ -6,12 +6,28 @@ const TOKEN_KEY = 'token';
 const EXPIRES_KEY = 'expires';
 const USER_KEY = 'user';
 
+const getLocalStorageItem = (key: string): string|null => {
+    return typeof window !== "undefined" ? localStorage.getItem(key) : null;
+}
+
+const setLocalStorageItem = (key: string, value: string): void => {
+    if(typeof window !== "undefined"){
+        localStorage.setItem(key, value);
+    }
+}
+
+const removeLocalStorageItem = (key: string): void => {
+    if(typeof window !== "undefined"){
+        localStorage.removeItem(key);
+    }
+}
+
 const defaultState: UserState = {
     jwt: {
-        expiresAt: parseInt(localStorage.getItem(EXPIRES_KEY) ?? '0'),
-        token: localStorage.getItem(TOKEN_KEY) ?? '',
+        expiresAt: parseInt(getLocalStorageItem(EXPIRES_KEY) ?? '0'),
+        token: getLocalStorageItem(TOKEN_KEY) ?? '',
     },
-    user: JSON.parse(localStorage.getItem(USER_KEY) ?? 'null'),
+    user: JSON.parse(getLocalStorageItem(USER_KEY) ?? 'null'),
 };
 
 const authSlice = createSlice({
@@ -25,17 +41,17 @@ const authSlice = createSlice({
             state.jwt.token = action.payload.token;
             state.jwt.expiresAt = action.payload.expires;
             state.user = action.payload.user;
-            localStorage.setItem(TOKEN_KEY, state.jwt.token);
-            localStorage.setItem(EXPIRES_KEY, state.jwt.expiresAt.toString());
-            localStorage.setItem(USER_KEY, JSON.stringify(state.user));
+            setLocalStorageItem(TOKEN_KEY, state.jwt.token);
+            setLocalStorageItem(EXPIRES_KEY, state.jwt.expiresAt.toString());
+            setLocalStorageItem(USER_KEY, JSON.stringify(state.user));
         },
         logoutUser(state) {
             state.jwt.token = "";
             state.jwt.expiresAt = 0;
             state.user = null;
-            localStorage.removeItem(TOKEN_KEY);
-            localStorage.removeItem(EXPIRES_KEY);
-            localStorage.removeItem(USER_KEY);
+            removeLocalStorageItem(TOKEN_KEY);
+            removeLocalStorageItem(EXPIRES_KEY);
+            removeLocalStorageItem(USER_KEY);
         },
     },
 });
